fff8e61e8e92df5bab3ed354f95181a891cd9d1f
Yogesh K <armsvcmode@gmail.com>
2022-08-13 18:40:21 +0530

    Uniswap - Swap example

---
+++ dapps/Uniswap-CoreConcepts/core_concepts/contracts/Swap.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import "@uniswap/v2-periphery/contracts/interfaces/IERC20.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IWETH.sol";
+
+contract CheckSwap {
+    address constant Uniswap_V2_Router =
+        0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address immutable Weth;
+
+    constructor() {
+        Weth = IUniswapV2Router02(Uniswap_V2_Router).WETH();
+    }
+
+    // This is to check if we have all the correct addresses
+    function get_contract_address() public view returns (address, address) {
+        return (Uniswap_V2_Router, Weth);
+    }
+
+    function simulateSwap(
+        address _tokenIn, // DAI
+        address _tokenOut, // ANT
+        uint256 _amountIn,
+        uint256 _amountOutMin,
+        address _to // address to where I should receive ANT tokens
+    ) external {
+        // transfer all token In to this contract
+        IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountIn);
+        // give approval for router to use the token in amount
+        IERC20(_tokenIn).approve(Uniswap_V2_Router, _amountIn);
+
+        address[] memory path;
+        if (_tokenIn == Weth || _tokenOut == Weth) {
+            path = new address[](2);
+            path[0] = _tokenIn;
+            path[1] = _tokenOut;
+        } else {
+            path = new address[](2);
+            path[0] = _tokenIn;
+            path[1] = _tokenOut;
+            /*
+            path = new address[](3);
+            path[0] = _tokenIn;
+            path[1] = Weth;
+            path[2] = _tokenOut;
+*/
+        }
+
+        IUniswapV2Router02(Uniswap_V2_Router).swapExactTokensForTokens(
+            _amountIn,
+            _amountOutMin,
+            path,
+            _to,
+            block.timestamp
+        );
+    }
+}
---
+++ dapps/Uniswap-CoreConcepts/core_concepts/test/testSwap.js
@@ -0,0 +1,51 @@
+const BN = require("bn.js");
+const IERC20 = artifacts.require("IERC20");
+const checkSwap = artifacts.require("CheckSwap");
+const { DAI, WETH, ANT, GTC, AAVE, TETHER,DAI_WHALE } = require("./config");
+
+
+contract("checkSwap", (accounts) =>{
+  const AMOUNT_IN = new BN(10).pow(new BN(18)).mul(new BN(1000)); // 1000 DAI
+  const AMOUNT_OUT_MIN = 1;
+  const TOKEN_IN = DAI;
+  const TOKEN_OUT = AAVE;//TETHER; // ; //ANT;
+  const TO = accounts[9];
+  const WHALE = DAI_WHALE;
+
+  let SwapInstance;
+  let tokenIn;
+  let tokenOut;
+
+  beforeEach(async () => {
+    tokenIn = await IERC20.at(TOKEN_IN); // create in token instance
+    tokenOut = await IERC20.at(TOKEN_OUT); // create out token instance
+    SwapInstance = await checkSwap.new(); // create Swap token instance
+    await tokenIn.approve(SwapInstance.address, AMOUNT_IN, { from: WHALE });
+  });
+
+ it("Deployed addresses", async () => {
+
+    console.log("tokenIn:",tokenIn.address);
+    console.log("tokenOut:",tokenOut.address);
+    console.log("Swap instance:",SwapInstance.address);
+    const {0: variable_1, 1: variable_2} = await SwapInstance.get_contract_address({from: accounts[0]});
+    console.log("Router, WETH :",variable_1, variable_2);
+    console.log("WHALE:",WHALE);
+     
+    await SwapInstance.simulateSwap(
+      tokenIn.address,
+      tokenOut.address,
+      AMOUNT_IN,
+      AMOUNT_OUT_MIN,
+      TO,
+      {
+        from: WHALE,
+      }
+    );
+
+    console.log(`in ${AMOUNT_IN}`);
+    console.log(`out ${await tokenOut.balanceOf(TO)}`);
+
+ });
+
+});
