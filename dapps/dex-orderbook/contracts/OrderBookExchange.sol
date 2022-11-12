496f50ce5d1f1e4841ce05abcbe29231c5970e43
Yogesh K <armsvcmode@gmail.com>
2022-11-12 12:58:49 +0530

    Deposit token and balance added

--- dapps/dex-orderbook/contracts/OrderBookExchange.sol
+++ dapps/dex-orderbook/contracts/OrderBookExchange.sol
@@ -1,13 +1,49 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: GPL-3.0
+/*-----------------------------------------------------------
+ @Filename:         OrderBookExchange.sol
+ @Copyright Author: Yogesh K
+ @Date:             11/11/2022
+-------------------------------------------------------------*/
 pragma solidity ^0.8.12;
 import "hardhat/console.sol";
+import "./TokenERC20.sol";
 
 contract OrderBookExchange {
     address public feeAccount;
     uint256 public feePercent;
+    // token addr => user addr => amount
+    mapping(address => mapping(address => uint256)) public usertokens;
+    event Deposit(address token, address user, uint256 amount, uint256 balance);
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
+    }
+
+    // Deposit tokens
+    function depositToken(address _token, uint256 _amount) public {
+        // Transfer tokens to exchange
+        require(
+            TokenERC20(_token).transferFrom(msg.sender, address(this), _amount),
+            "transfer failed"
+        );
+        // Update user balance
+        usertokens[_token][msg.sender] += _amount;
+        // Emit event
+        emit Deposit(
+            _token,
+            msg.sender,
+            _amount,
+            usertokens[_token][msg.sender]
+        );
+    }
+
+    // Check balances
+    function balanceOf(address _token, address _user)
+        public
+        view
+        returns (uint256)
+    {
+        return usertokens[_token][_user];
     }
 }
--- dapps/dex-orderbook/test/OrderBookExchange.js
+++ dapps/dex-orderbook/test/OrderBookExchange.js
@@ -1,3 +1,8 @@
+/*-----------------------------------------------------------
+ @Filename:         OrderBookExchange.js
+ @Copyright Author: Yogesh K
+ @Date:             11/11/2022
+-------------------------------------------------------------*/
 const { ethers } = require("hardhat");
 const { expect } = require('chai');
 
@@ -11,13 +16,21 @@ describe('OrderBookExchange', () =>{
     const feePercent = 10
 
     beforeEach(async ()=>{
+        const Exchange =  await ethers.getContractFactory('OrderBookExchange');
+        const Token =  await ethers.getContractFactory('TokenERC20');
+
         accounts = await ethers.getSigners()
         deployer = accounts[0]
         feeAccount = accounts[1]
+        user1 = accounts[2]
 
-        const Exchange =  await ethers.getContractFactory('OrderBookExchange');
         // get deployed instance of contract
         exchange = await Exchange.deploy(feeAccount.address, feePercent)
+        token1 = await Token.deploy('KN Token', 'KNT', 1000000) // 1 million tokens
+
+        // transfer some tokens to user1
+        let tx = await token1.connect(deployer).transfer(user1.address,tokens(100))
+        await tx.wait() // wait for tx to mine
     })
 
     describe('Deployment', ()=>{
@@ -27,5 +40,41 @@ describe('OrderBookExchange', () =>{
         it('tracks the fee percent',async ()=>{
             expect(await exchange.feePercent()).to.equal(feePercent);
         })
-  })
+    })
+
+    describe('Depositing tokens', ()=>{
+        let tx, result
+        let amount = tokens(10)
+        describe('Success', () =>{
+            beforeEach(async() =>{
+                // Approve token
+                tx = await token1.connect(user1).approve(exchange.address,amount)
+                result = await tx.wait() // wait for tx to mine
+                // Deposit token
+                tx = await exchange.connect(user1).depositToken(token1.address,amount)
+                result = await tx.wait() // wait for tx to mine
+            })
+            it('tracks token deposit', async()=>{
+                expect(await token1.balanceOf(exchange.address)).to.equal(amount)
+                expect(await exchange.usertokens(token1.address, user1.address)).to.equal(amount)
+                expect(await exchange.balanceOf(token1.address, user1.address)).to.equal(amount)
+            })
+            it('emits a Deposit event', async ()=>{
+                const event = result.events[1] //2 events are emitted
+                expect(event.event).to.equal('Deposit')
+
+                const args = event.args
+                expect(args.token).to.equal(token1.address)
+                expect(args.user).to.equal(user1.address)
+                expect(args.amount).to.equal(amount)
+                expect(args.balance).to.equal(amount)
+            })
+        })
+        describe('Failure', () =>{
+
+            it('fails when no tokens are approved',async ()=>{
+               await expect(exchange.connect(user1).depositToken(token1.address,amount)).to.be.reverted
+            })
+       })
+    })
 })
