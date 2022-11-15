b356d3dca3e828f0acca46233c50b52de1bb0300
Yogesh K <armsvcmode@gmail.com>
2022-11-15 19:40:52 +0530

    Seed Exchange with some preorders

--- dapps/dex-orderbook/scripts/1_deploy.js
+++ dapps/dex-orderbook/scripts/1_deploy.js
@@ -1,13 +1,30 @@
-
 async function main() {
-  const Token = await ethers.getContractFactory("TokenERC20");
+  console.log('Preparing for deployment')
+  // Fetch contract before deploy
+  const Token = await ethers.getContractFactory("TokenERC20")
+  const Exchange = await ethers.getContractFactory("OrderBookExchange")
 
-  const token = await Token.deploy();
-  await token.deployed();
+  // fetch accounts
+  const accounts = await ethers.getSigners()
+  console.log(`Accounts:\n ${accounts[0].address}\n ${accounts[1].address}`)
 
-  console.log(
-    `ERC20 Token deployed at: ${token.address}`
-  );
+  // Deploy contracts
+  const kn = await Token.deploy('KN Token', 'KNT', 1000000)
+  await kn.deployed()
+  console.log(`KN deployed at: ${kn.address}`)
+
+  const fETH = await Token.deploy('fETH', 'fETH',1000000)
+  await fETH.deployed()
+  console.log(`fETH deployed at: ${fETH.address}`)
+
+  const fDAI = await Token.deploy('fDAI', 'fDAI',1000000)
+  await fDAI.deployed()
+  console.log(`fDAI deployed at: ${fDAI.address}`)
+
+
+  const exchange = await Exchange.deploy(accounts[1].address, 10)
+  await exchange.deployed()
+  console.log(`Exchange deployed at: ${exchange.address}`)
 }
 
 main().catch((error) => {
