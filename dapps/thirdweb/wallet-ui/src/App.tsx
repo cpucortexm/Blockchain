7b60eec9552579da29efece81a2643e7dc735d2d
Yogesh K <armsvcmode@gmail.com>
2023-03-21 17:41:40 +0530

    Nav bar implemented

---
+++ dapps/thirdweb/wallet-ui/src/App.tsx
@@ -0,0 +1,10 @@
+import { Nav } from "./components/Nav";
+import "./styles/Home.css";
+
+export default function Home() {
+  return (
+    <div className="container">
+        <Nav/>
+   </div>
+  );
+}
---
+++ dapps/thirdweb/wallet-ui/src/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+
+// This is the chain your dApp will work on.
+// Change this to the chain your app is built for.
+// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
+// const activeChain = "ethereum";
+
+// Note: We are using our own local custom chain
+const activeChain = {
+        // === Required information for connecting to the network === \\
+        chainId: 313, // Chain ID of the network
+        // Array of RPC URLs to use
+        rpc: ["HTTP://127.0.0.1:8545"],
+
+        // === Information for adding the network to your wallet (how it will appear for first time users) === \\
+        // Information about the chains native currency (i.e. the currency that is used to pay for gas)
+        nativeCurrency: {
+          decimals: 18,
+          name: "",
+          symbol: "ETH",
+        },
+        shortName: "", // Display value shown in the wallet UI
+        slug: "", // Display value shown in the wallet UI
+        testnet: true, // Boolean indicating whether the chain is a testnet or mainnet
+        chain: "", // Chain
+        name: "ganache-local", // Name of the network
+      }
+
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
+  <React.StrictMode>
+    <ThirdwebProvider activeChain={activeChain}>
+      <App />
+    </ThirdwebProvider>
+  </React.StrictMode>
+);
+
