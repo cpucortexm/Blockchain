58ec12ed7bfe29ea11ac851e9e571593016571ed
Yogesh K <armsvcmode@gmail.com>
2023-03-23 13:23:16 +0530

    completed first row of the card for erc20 and erc721 tokens

--- dapps/thirdweb/wallet-ui/src/components/ERC20TokensCard.tsx
+++ dapps/thirdweb/wallet-ui/src/components/ERC20TokensCard.tsx
@@ -1,8 +1,19 @@
 
+import { useAddress } from "@thirdweb-dev/react";
+
 export const ERC20TokensCard = () =>{
+    const address = useAddress();
     return(
         <div className="token-card">
-            Content box
+            <div className="first-row">
+                <span className="eth-account"><strong>ETH Account: </strong>{address? address : " not connected"}</span>
+            </div>
+            <div className="second-row">
+               <span>Symbol</span>
+                <span>Token</span>
+                <span>Amount</span>
+            </div>
+            <div className="third-row"></div>
         </div>
     )
 }
--- dapps/thirdweb/wallet-ui/src/components/ERC721TokensCard.tsx
+++ dapps/thirdweb/wallet-ui/src/components/ERC721TokensCard.tsx
@@ -1,7 +1,17 @@
+import { useAddress } from "@thirdweb-dev/react"
+
 export const ERC721TokensCard = () =>{
+    const address = useAddress()
     return(
         <div className="token-card">
-            Content box
+             <div className="first-row">
+                <span className="eth-account"><strong>ETH Account: </strong>{address? address : " not connected"}</span>
+            </div>
+            <div className="second-row">
+               <span>Symbol</span>
+                <span>Token</span>
+                <span>MetaData</span>
+            </div>
         </div>
     )
 }
